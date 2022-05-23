d3951d83b67768baaebe8f731da1af49c216bb3d
Jocelyn <jocelyn@kinshasadigital.com>
2022-05-23 17:30:24 +0100

    feat: complete loop 3 challenge

--- Loops.java
+++ Loops.java
@@ -27,19 +27,32 @@ public class Loops {
             }
 
             System.out.println();
+
         }
 
-        // Exercise 3
-        for (int i = 6; i > 1; i--) {
-            for (int j = 1; j < i; j++) {
-                System.out.print(j);
-            }
+        System.out.println();
 
-            for (int k = 6; k >= i; k--) {
-                System.out.print("&");
+        // Exercise 3
+
+        final int MAX_NUMBER = 6;
+
+        for (int i = 1; i <= MAX_NUMBER; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i < MAX_NUMBER) {
+                    if (j == 1) {
+                        System.out.print("@");
+                    } else if (j == i) {
+                        System.out.print(" @");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    System.out.print("@");
+                }
             }
 
             System.out.println();
         }
+
     }
 }
