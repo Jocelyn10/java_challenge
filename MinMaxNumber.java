fedce0f26edfa5ac2dd77e9bcb822e3bb0d47b05
Jocelyn10 <jocelynnsa@gmail.com>
2022-06-21 22:33:52 +0100

    complete MinMaxNumber challenge

---
+++ MinMaxNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MinMaxNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 2, small = 0, large = 0;
+
+        System.out.print("Num 1 : ");
+        int a = sc.nextInt();
+        small = a;
+        large = a;
+
+        while (i <= 10) {
+            System.out.println("Num " + i + " : ");
+            int b = sc.nextInt();
+
+            if (b < small) {
+                small = b;
+            }
+
+            if (b > large) {
+                large = b;
+            }
+
+            i++;
+        }
+
+        System.out.print("Larger : " + large + "  Smaller : " + small);
+    }
+}
