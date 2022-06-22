cef11b597d9d43678f24f8d28db40df72669f20f
Jocelyn10 <jocelynnsa@gmail.com>
2022-06-22 13:05:43 +0100

    create loop exercice 5 challenge

--- Loops.java
+++ Loops.java
@@ -54,5 +54,54 @@ public class Loops {
             System.out.println();
         }
 
+        System.out.println();
+
+        // Exercise 4
+
+        for (int i = 6; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 6; k >= i; k--) {
+                System.out.print("$");
+            }
+
+            for (int k = 6; k >= i; k--) {
+                System.out.print("$");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Exercise 5
+
+        System.out.println("      #");
+
+        for (int i = 6; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.print("#");
+
+            for (int k = 6; k >= i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 6; k >= i; k--) {
+                System.out.print(" ");
+            }
+
+            System.out.print("#");
+            System.out.println();
+        }
+
+        for (int k = 7; k >= 0; k--) {
+            System.out.print("# ");
+        }
+
     }
 }
