499b09fed4f1ecd1c85c327ec864dfacba415084
Jocelyn10 <jocelynnsa@gmail.com>
2022-06-24 18:07:21 +0100

    create reverse values inside array

---
+++ DuplicateNumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class DuplicateNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        System.out.println("Array values : ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Num 1 : ");
+        int a = sc.nextInt();
+        System.out.println("Num 2 : ");
+        int b = sc.nextInt();
+
+        int index_a = 0, index_b = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == a) {
+                index_a = i;
+            }
+
+            if (arr[i] == b) {
+                index_b = i;
+            }
+        }
+
+        int c = arr[index_a];
+        arr[index_a] = arr[index_b];
+        arr[index_b] = c;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "  ");
+        }
+
+    }
+}
