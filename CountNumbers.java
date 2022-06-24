ef2fc7596baf208e3f16649320941288a06e6e5e
Jocelyn10 <jocelynnsa@gmail.com>
2022-06-24 17:06:18 +0100

    rename to count of numbers

--- CountNumbers.java
+++ CountNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumAndMultiplication {
+public class CountNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
