55d87ff689147057229fb852187d7ad121728286
Jocelyn10 <jocelynnsa@gmail.com>
2022-06-24 18:15:56 +0100

    rename file

--- ReverseValuesInsideArray.java
+++ ReverseValuesInsideArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DuplicateNumber {
+public class ReverseValuesInsideArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
